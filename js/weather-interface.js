12ae52d3bc20fa3c814381c81579c529931a21a0
Nhat Hoang <hoangnh092185@gmail.com>
2016-10-10 22:26:09 -0700

    add more api functionality to the weather forecast.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,26 @@
-var apiKey = "http://api.openweathermap.org/data/2.5/weather?q=Portland,us&appid=44db6a862fba0b067b1930da0d769e98";
+var apiKey = "9b3ec90fdf286bb3373b22f0a9b2ba65
+";
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     var city = $('#location').val();
+//     $('#location').val("");
+//     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+//       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+//       console.log(JSON.stringify(response));
+//     });
+//     console.log("Notice: The GET request has been made.");
+//   });
+// });
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
     });
   });
 });
